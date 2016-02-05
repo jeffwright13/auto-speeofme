f2bafcbf69b191f65d08d54b9c48b55ac8deccf1
Jeff Wright <jeff.washcloth@gmail.com>
2016-02-05 14:44:05 -0500

    Marginally functional. Launches browser, goes to URL and checks title, then kills browser.

--- auto.py
+++ auto.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python
 
 def main():
-        from selenium import webdriver
+    from selenium import webdriver
+    
+    base_url = "http://speedof.me"
+    title = "SpeedOf.Me"
+    
+    # Launch browser using Selenium driver
+    browser = webdriver.Firefox()
+    browser.get(base_url)
+    assert title in browser.title
+    
+    # Kill browser
+    browser.quit()
 
 if __name__ == '__main__':
     main()
